1b5c60678cd1c0704f33dd78762eede5ff6adcb7
fei19910716 <wsWS706224>
2021-10-30 21:00:20 +0800

    使用constant buffer绘制立方体，每个面颜色均不同

--- src/Graphics/Graphics.cpp
+++ src/Graphics/Graphics.cpp
@@ -84,27 +84,25 @@ void Graphics::DrawTestTriangle(float angle,float x, float y){
 
 	struct Vertex
 	{
-		struct 
+		struct
 		{
 			float x;
 			float y;
+			float z;
 		} pos;
-		
-		struct 
-		{
-			unsigned char r,g,b,a;
-		} color;
 	};
 
 	// create vertex buffer (1 2d triangle at center of screen)
 	Vertex vertices[] =
 	{
-		{ 0.0f,0.5f,255,0,0,0 },
-		{ 0.5f,-0.5f,0,255,0,0 },
-		{ -0.5f,-0.5f,0,0,255,0 },
-		{ -0.3f,0.3f,0,255,0,0 },
-		{ 0.3f,0.3f,0,0,255,0 },
-		{ 0.0f,-1.0f,255,0,0,0 },
+		{ -1.0f,-1.0f,-1.0f	 },
+		{ 1.0f,-1.0f,-1.0f	 },
+		{ -1.0f,1.0f,-1.0f	 },
+		{ 1.0f,1.0f,-1.0f	  },
+		{ -1.0f,-1.0f,1.0f	 },
+		{ 1.0f,-1.0f,1.0f	  },
+		{ -1.0f,1.0f,1.0f	 },
+		{ 1.0f,1.0f,1.0f	 },
 	};
 	wrl::ComPtr<ID3D11Buffer> pVertexBuffer;
 	D3D11_BUFFER_DESC bd = {};
@@ -150,8 +148,7 @@ void Graphics::DrawTestTriangle(float angle,float x, float y){
 	// input (vertex) layout (2d position only)
 	wrl::ComPtr<ID3D11InputLayout> pInputLayout;
 	const D3D11_INPUT_ELEMENT_DESC ied[] = {
-		{"Position",0,DXGI_FORMAT_R32G32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0},
-		{"Color",0,DXGI_FORMAT_R8G8B8A8_UNORM,0,8,D3D11_INPUT_PER_VERTEX_DATA,0}
+		{"Position",0,DXGI_FORMAT_R32G32B32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0},
 	};
 	GFX_THROW_INFO(pDevice->CreateInputLayout(
 		ied,std::size(ied),pBlob->GetBufferPointer(),
@@ -162,10 +159,12 @@ void Graphics::DrawTestTriangle(float angle,float x, float y){
 
 	// create index buffer
 	const unsigned short indices[] = {
-		0,1,2,
-		0,2,3,
-		0,4,1,
-		2,1,5,
+		0,2,1, 2,3,1,
+		1,3,5, 3,7,5,
+		2,6,3, 3,6,7,
+		4,5,7, 4,7,6,
+		0,4,2, 2,4,6,
+		0,1,4, 1,5,4
 	};
 	wrl::ComPtr<ID3D11Buffer> pIndexBuffer;
 	D3D11_BUFFER_DESC ibd = {};
@@ -197,8 +196,9 @@ void Graphics::DrawTestTriangle(float angle,float x, float y){
 		{
 			dx::XMMatrixTranspose(
 				dx::XMMatrixRotationZ( angle ) *
-				dx::XMMatrixScaling( 3.0f / 4.0f,1.0f,1.0f ) *
-				dx::XMMatrixTranslation( x,y,0.0f )
+				dx::XMMatrixRotationX( angle ) *
+				dx::XMMatrixTranslation( x,y,4.0f ) *
+				dx::XMMatrixPerspectiveLH( 1.0f,3.0f / 4.0f,0.5f,10.0f )
 			)
 		}
 	};
@@ -223,6 +223,39 @@ void Graphics::DrawTestTriangle(float angle,float x, float y){
 		1u,
 		pConstantBuffer.GetAddressOf()
 	);
+
+	// lookup table for cube face colors
+	struct ConstantBuffer2{
+		struct{
+			float r,g,b,a;
+		} face_colors[6];
+	};
+	const ConstantBuffer2 cb2={
+		{
+			{1.0f,0.0f,1.0f},
+			{1.0f,0.0f,0.0f},
+			{0.0f,1.0f,0.0f},
+			{0.0f,0.0f,1.0f},
+			{1.0f,1.0f,0.0f},
+			{0.0f,1.0f,1.0f},
+		}
+	};
+
+	wrl::ComPtr<ID3D11Buffer> pConstantBuffer2;
+	D3D11_BUFFER_DESC cbd2;
+	cbd2.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	cbd2.Usage = D3D11_USAGE_DEFAULT;
+	cbd2.CPUAccessFlags = 0u;
+	cbd2.MiscFlags = 0u;
+	cbd2.ByteWidth = sizeof( cb2 );
+	cbd2.StructureByteStride = 0u;
+	D3D11_SUBRESOURCE_DATA csd2 = {};
+	csd2.pSysMem = &cb2;
+	GFX_THROW_INFO( pDevice->CreateBuffer( &cbd2,&csd2,&pConstantBuffer2 ) );
+
+	// bind constant buffer to pixel shader
+	pContext->PSSetConstantBuffers( 0u,1u,pConstantBuffer2.GetAddressOf() );
+
 
 
 	// create pixel shader
